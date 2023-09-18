51ea9bdb56d92a620363888b78f3e7421feab57a
eyal <eyal@api.com>
2023-09-18 12:21:08 +0300

    "start working with v 2.2"

--- app/main.py
+++ app/main.py
@@ -9,49 +9,45 @@ def new_func(app):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3001)
 
 
 # Load data from the JSON file
 with open('book-dict.json', 'r') as file:
     book_dict = json.load(file)
 
-# Define API endpoints
+# Define API endpoints:
+#root
 @app.get("/")
 def get_intro():
-    return "Welcome to the Book API!"
+    return ["Welcome to the Book API!" ,
+    {"server": "author API Server",
+     "version": "1.0",
+     "status": "Running"}
+     ]
 
+#authors
 @app.get("/author/{author_name}")
 def get_author(author_name: str):
     try:
         # Check if the author_name is present in the book_dict
         if any(book['author'] == author_name for book in book_dict):
             author_books = [book['title'] for book in book_dict if book['author'] == author_name]
-            author_info = {
+            author_info = [{
                 'name': author_name,
                 'books': author_books
-            }
+            },
+            {   "server": "author API Server",
+                "version": "1.0",
+                "status": "Running"
+            }]
             return author_info
         else:
             raise HTTPException(status_code=404, detail="Author not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-#@app.get("/book/{book_title}")
-#def get_book(book_title: str):
-    try:
-        if any(book['title'] == book_title for book in book_dict):
-            book_info =  {
-                'book': book_title,
-                'genre': book[0]['genre'],
-                'author': book[0]['author']
-            }
-            return book_info
-        else:
-            raise HTTPException(status_code=404, detail="book not found")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-    
+#books
 @app.get("/book/{book_title}")
 def get_book(book_title: str):
     try:
@@ -59,11 +55,16 @@ def get_book(book_title: str):
         matching_books = [book for book in book_dict if book['title'] == book_title]
         if matching_books:
             # Assuming book titles are unique, return the first match
-            book_info = {
+            book_info = [{
                 'book': matching_books[0]['title'],
                 'genre': matching_books[0]['genre'],
                 'author': matching_books[0]['author']
-            }
+            },
+            {
+                 "server": "Book API Server",
+                 "version": "1.0",
+                 "status": "Running"
+            }]
             return book_info
         else:
             raise HTTPException(status_code=404, detail="Book not found")
